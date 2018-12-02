3945daab2329d023ea9d5f61b636d3f43b97e19f
ShellyE <shelly.einer@gmail.com>
2018-12-02 22:06:39 +0200

    added sign up function in server itself, corrections: now messages queue contains tuple of client_socket and message, and functions receive sqlite database connection

--- Application/sqlite_database.py
+++ Application/sqlite_database.py
@@ -1,6 +1,22 @@
 import sqlite3
 import os
 
+def does_user_exist(db_connection, phone_num):
+	'''
+		Function checks if user (phone number) exists in database
+		Input: database connection, phone number to search
+		Output: True if phone number exists in database, False otherwise
+	'''
+	pass
+
+def sign_up(db_connection, phone_num, password, name):
+	'''
+		Function adds new user to users table
+		Input: database connection, phone number, password and name of the user
+		Output: None
+	'''
+	pass
+
 def init_and_load(database_name):
 	'''
 		Function loads sql database and intializes it if the database doesn't exist already
@@ -10,10 +26,10 @@ def init_and_load(database_name):
 	database_exists = os.path.isfile(database_name)
 	connection = sqlite3.connect(database_name)
 	if not database_exists: # Init tables for the first time database created
-		connection_cursor = conn.cursor()
+		connection_cursor = connection.cursor()
 		tables_list = ["CREATE TABLE USERS(PHONE_NUM TEXT PRIMARY KEY NOT NULL, PASSWORD TEXT NOT NULL, NAME TEXT NOT NULL)",
 						"CREATE TABLE MESSAGES(MESSAGE_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, SRC_PHONE TEXT NOT NULL, DEST_PHONE TEXT NOT NULL, MESSAGE TEXT NOT NULL)"]
 		for table in tables_list:
-			c.execute(table)
+			connection_cursor.execute(table)
 	return connection
 	
