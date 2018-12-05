de173a1f95ad037d6167ae9a1e35d5ce11eec967
ShellyE <shelly.einer@gmail.com>
2018-12-05 13:44:52 +0200

    Send message (server side) completed (almost: without speech recognition), and logical change in client handler

--- Application/Server/server.py
+++ Application/Server/server.py
@@ -7,7 +7,7 @@ import sqlite_database as sql_db
 PORT_NUM = 3124
 
 MAX_QUEUE_CONNECTIONS = 5
-MAX_MESSAGE_LEN = 32768
+MAX_SIZE_LEN = 10
 
 MESSAGES_QUEUE = deque()
 CONNECTED_CLIENTS = {}
@@ -25,21 +25,11 @@ GENERAL_ERROR_CODE = 0
 DETAILS_MISSING_ERROR_CODE = 1
 PHONE_EXISTS_ERROR_CODE = 2
 
-SOURCE_NOT_FOUND_ERROR_CODE = 4 # DETAILS MISSING ERROR COMBINED
-DESTINATION_UNREACHABLE_ERROR_CODE = 5
+SOURCE_INVALID_ERROR_CODE = 5
+DESTINATION_UNREACHABLE_ERROR_CODE = 6
 
 def is_user_connected(client_socket):
 	return client_socket in CONNECTED_CLIENTS.values()
-	
-def set_receive_message_ans(ans_dict, src_phone, text_message):
-	'''
-		Function sets receive message answer
-		Input: Answer message dict and message details: source and the message
-		Output: None
-	'''
-	ans_dict["code"] = RECEIVE_MESSAGES_CODE # Set message code (preset)
-	ans_dict["src_phone"] = src_phone # Set sender phone number
-	ans_dict["content"] = text_message
 
 def send_voice_message(db_connection, client_socket, message_dict):
 	'''
@@ -50,26 +40,25 @@ def send_voice_message(db_connection, client_socket, message_dict):
 				* checking: all the necessary information included in the message, if both src and dst phones exist in db
 		Output: Answer message dict
 	'''
-	ans_messages_dict = { client_socket: {} }
+	ans_messages_dict = {}
 	if not ("src_phone" in message_dict and "dst_phone" in message_dict and "content" in message_dict): # Checks if details are missing
-		ans_messages_dict[client_socket]["code"] = DETAILS_MISSING_ERROR_CODE
+		ans_messages_dict[client_socket] = { "code": DETAILS_MISSING_ERROR_CODE }
 		
-	elif not sql_db.does_user_exist(message_dict["src_phone"]): # Checks if source phone exists in database
-		ans_messages_dict[client_socket]["code"] = SOURCE_NOT_FOUND_ERROR_CODE
+	elif message_dict["src_phone"] not in CONNECTED_CLIENTS or CONNECTED_CLIENTS[message_dict["src_phone"]] != client_socket: # Checks if source phone is client's true phone
+		ans_messages_dict[client_socket] = { "code": SOURCE_INVALID_ERROR_CODE }
 		
-	elif not sql_db.does_user_exist(message_dict["dst_phone"): # Checks if destination phone exists in database
-		ans_messages_dict[client_socket]["code"] = DESTINATION_UNREACHABLE_ERROR_CODE
+	elif not sql_db.does_user_exist(db_connection, message_dict["dst_phone"]): # Checks if destination phone exists in database
+		ans_messages_dict[client_socket] = { "code": DESTINATION_UNREACHABLE_ERROR_CODE }
 		
 	else:
 		# Speech to text - to do
 		text_message = "test message"
+		# Set message that will be returned to sender and receiver (including code, message source and the text message)
+		ans_messages_dict[client_socket] = { "code": RECEIVE_MESSAGES_CODE, "messages": [{ "src_phone": message_dict["src_phone"], "dst_phone": message_dict["dst_phone"], "content": text_message }] }
 		
-		# Save sent message in text form, will be returned to sender as well
-		set_receive_message_ans(ans_messages_dict[client_socket], message_dict["src_phone"], text_message)
-		
-		if message_dict["dst_phone"] in CONNECTED_CLIENTS: # Checks if destination connected			
-			# Save new answer message (the sent message in text form) - to destination
-			ans_messages_dict[CONNECTED_CLIENTS[message_dict["dst_phone"]]] = ans_messages_dict[client_socket] # Shallow copy - is it OK?
+		if message_dict["dst_phone"] in CONNECTED_CLIENTS: # Destination connected, send answer to destination
+			# Copy message that will be returned to sender and receiver
+			ans_messages_dict[CONNECTED_CLIENTS[message_dict["dst_phone"]]] = ans_messages_dict[client_socket]
 		
 		else: # Destination disconnected, save message to sql database
 			sql_db.save_text_message(db_connection, message_dict["src_phone"], message_dict["dst_phone"], text_message)
@@ -90,15 +79,15 @@ def sign_up(db_connection, client_socket, message_dict):
 				* checking: all the necessary information included in the message, does user exist.
 		Output: Answer messages dict
 	'''
-	ans_messages_dict = { client_socket: {} }
+	ans_messages_dict = {}
 	if not ("phone" in message_dict and "password" in message_dict and "name" in message_dict): # Checks if details are missing
-		ans_messages_dict[client_socket]["code"] = DETAILS_MISSING_ERROR_CODE
+		ans_messages_dict[client_socket] = { "code": DETAILS_MISSING_ERROR_CODE }
 		
 	elif sql_db.does_user_exist(db_connection, message_dict["phone"]): # Checks if phone number already exists
-		ans_messages_dict[client_socket]["code"] = PHONE_EXISTS_ERROR_CODE
+		ans_messages_dict[client_socket] = { "code": PHONE_EXISTS_ERROR_CODE }
 		
 	else: # Passed all the checks, new user is added
-		ans_messages_dict[client_socket]["code"] = SIGN_UP_CODE
+		ans_messages_dict[client_socket] = { "code": SIGN_UP_CODE }
 		sql_db.sign_up(db_connection, message_dict["phone"], message_dict["password"], message_dict["name"])
 		
 	return ans_messages_dict
@@ -120,9 +109,16 @@ def client_handler(client_socket):
 		Output: None
 	'''
 	try:
-		# Receiving data from the client
-		client_message = client_socket.recv(MAX_MESSAGE_LEN)
-		MESSAGES_QUEUE.append((client_socket, client_message))
+		while True:
+			# Receiving data size from client
+			message_size = int(client_socket.recv(MAX_SIZE_LEN))
+		
+			# Receiving data from the client
+			client_message = client_socket.recv(message_size)
+		
+			# Add message to messages queue with client's socket
+			MESSAGES_QUEUE.append((client_socket, client_message))
+		
 	# Sending data back
 	#client_socket.sendall("got it!")
 	
@@ -138,9 +134,10 @@ def listen_and_accept(listening_socket):
 	Input: listening socket
 	Output: None
 	'''
-	# Create a new conversation socket
 	while True:
+		# Create a new conversation socket
 		client_socket, client_address = listening_socket.accept()
+		# Start thread for client accepted
 		thread.start_new_thread(client_handler, (client_socket, ))
 
 def bind():
@@ -163,6 +160,7 @@ def main():
 	sql_database = sql_db.init_and_load('SpeakToMe.db')
 	try:
 		listening_socket = bind()
+		
 	except Exception, e:
 		print e
 		return 0
@@ -170,14 +168,19 @@ def main():
 	try:
 		thread.start_new_thread(handle_requests, (sql_database, ))
 		listen_and_accept(listening_socket)
-		# Test Example
-		# print sign_up(sql_database, 8537, {"code": SIGN_UP_CODE, "phone": "0503885746", "password":"cool", "name":"Shelly"})
+		
+		# Test Examples:
+		# print sign_up(sql_database, 8538, {"code": SIGN_UP_CODE, "phone": "0539948875", "password":"cool2", "name":"Netanel"})
+		# CONNECTED_CLIENTS["0548827476"] = 8537
+		# print send_voice_message(sql_database, 8537, {"code": SEND_VOICE_MESSAGE_CODE, "src_phone": "0548827476", "dst_phone": "0539948875", "content": "53478573485783447893"})
+		
 	except Exception, e:
 		print e	
+		
 	finally:
 		# Closing the listening socket
 		listening_socket.close()
 		# Closing the sqlite database connection
 		sql_database.close()
-		
+	
 main()
